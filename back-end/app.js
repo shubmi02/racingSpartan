0d0db93249d902d4c60e718093ada7eabe33abb2
Pajay1234 <74382643+Pajay1234@users.noreply.github.com>
2024-01-20 00:55:36 -0800

    good stuff
    router
    db connection
    concurrent npm start

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const dotenv = require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 var app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+connectToDatabase();
+
+const userDB = client.db('RacingSpartan').collection('Users'); 
 
 app.use(cors({
   origin: true,
@@ -14,8 +35,17 @@ app.use(cors({
 app.use(bodyParser.json({ limit: '10mb' }));
 
 
+
+
 app.post('/api/test', async (req, res) => {
-  res.json('hows it goin');
+  const doc = {
+    name: 'kanoa',
+    age: '25'
+  }
+
+  const result = await userDB.insertOne(doc);
+  console.log(`inserted id: ${result.insertedId}`);
+  res.json('hi');
 });
 
 app.listen(port, () => console.log(`server started on port ${port}`));
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 
 function App() {
-
   async function testGet(e) {
     e.preventDefault();
     let response = await axios.post('http://localhost:5000/api/test');
     console.log(response.data);
   }
+
   return (
     <div className="App">
-      <button onClick={async (e)=> await testGet(e)}>cock</button>
+       <button onClick={async (e)=> await testGet(e)}>cock</button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/homepage" />} />
+          {/* <Route path="/homepage" element={<SquishList />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
