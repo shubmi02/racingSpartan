7618db114275b513a495482c2bd98936b74b658c
shubhammishra-2020 <shubham.mishra@sjsu.edu>
2024-01-19 19:36:58 -0800

    added reactapp

--- dist/index.js
+++ dist/index.js
@@ -17,3 +17,29 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+// Array of example users for testing purposes
+const users = [
+    {
+        id: 1,
+        name: 'Maria Doe',
+        email: 'maria@example.com',
+        password: 'maria123'
+    },
+    {
+        id: 2,
+        name: 'Juan Doe',
+        email: 'juan@example.com',
+        password: 'juan123'
+    }
+];
+// route login
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const user = users.find(user => {
+        return user.email === email && user.password === password;
+    });
+    if (!user) {
+        return res.status(404).send('User Not Found!');
+    }
+    return res.status(200).json(user);
+});
--- index.ts
+++ index.ts
@@ -21,3 +21,40 @@
     console.log(`Example app listening on port ${port}`)
   });
   
+  interface FormInputs {
+    email: string,
+    password: string
+  }
+
+  // Array of example users for testing purposes
+  const users = [
+    {
+      id: 1,
+      name: 'Maria Doe',
+      email: 'maria@example.com',
+      password: 'maria123'
+    },
+    {
+      id: 2,
+      name: 'Juan Doe',
+      email: 'juan@example.com',
+      password: 'juan123'
+    }
+  ];
+
+  // route login
+  app.post('/login', (req: Request, res: Response) => {
+    const { email, password }:FormInputs = req.body;
+
+    const user = users.find(user => {
+      return user.email === email && user.password === password
+    });
+
+    if (!user) {
+      return res.status(404).send('User Not Found!')
+    }
+
+    return res.status(200).json(user)
+  });
+  
+  
