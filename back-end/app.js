c2bc9b7f304f884c514a81b5589f89748fab9227
Pajay1234 <74382643+Pajay1234@users.noreply.github.com>
2024-01-19 22:05:57 -0800

    simplified backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,42 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testbackend = require("./routes/test-backend.js");
+
 var app = express();
+const port = 5000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(cors({
+  origin: true,
+  maxAge: 86400
+}));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({ limit: '10mb' }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testbackend", testbackend);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+app.post('/test', async (req, res) => {
+  res.json('hows it goin');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(port, () => console.log(`server started on port ${port}`));
 
 
-module.exports = app;
+
+
